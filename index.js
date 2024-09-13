70162d9823b8a2b61b34b920728882ab6a2f41c6
royrussell7 <123126650+royrussell7@users.noreply.github.com>
2024-09-13 12:27:03 -0400

    finished

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-let num1 , num2, operation, answer, ; 
+let num1 , num2, operation, answer ; 
+console.log ("This is a calculator!");
+console.log ("This calulator can do addition, subtraction, mulitplication, division, and powers, just put two numbers and the operation in!");
+console.log ("For squareroot and trigometric functions (sin, cos, tan), input anything for the second value. Trigonometrics show up as radians.")
 x = parseInt(prompt ("First number")) ; 
 operation = prompt ("What operation do you want done?") ; 
 y = parseInt (prompt ("Second number")) ; 
@@ -20,6 +23,27 @@ function division (num1, num2) {
     answer = num1 / num2
 
 }
+
+function power (num1, num2) {
+    answer = num1**num2
+}
+
+function squareroot (num1) {
+answer = Math.sqrt(num1);
+}
+
+function sin (num1) { 
+    answer = Math.sin(num1);
+}
+
+function cos (num1) { 
+    answer = Math.cos(num1);
+}
+
+function tan (num1) { 
+    answer = Math.tan(num1);
+}
+
 
 
 
@@ -44,3 +68,25 @@ division (x,y);
 console.log (answer) ; 
     }
 
+else if (operation == "power") {
+    power (x,y); 
+    console.log (answer) ; 
+            }
+ else if (operation == "squareroot") {
+squareroot (x); 
+console.log (answer) ; 
+                        }
+else if (operation == "sin") { 
+    sin (x);
+    console.log (answer);
+}
+
+else if (operation == "cos") { 
+    cos(x);
+    console.log (answer);
+}
+
+else if (operation == "tan") { 
+    tan(x);
+    console.log (answer);
+}
